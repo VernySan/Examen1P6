5b369f2051a8d91428887e498d9b89be3016e950
verny <verny.mata@icumed.com>
2021-10-22 19:59:52 -0600

    Add project files.

---
+++ Database/InitialData/ScriptConfiguration/01-TipoInquilino.sql
@@ -0,0 +1,40 @@
+﻿DROP TABLE IF EXISTS #TipoInquilinoTemp
+
+SELECT 
+Id_TipoInquilino, Descripcion, Estado INTO #TipoInquilinoTemp
+FROM (
+VALUES
+(1, 'Físico',1),
+(2, 'Jurídico',1)
+)AS TEMP (Id_TipoInquilino, Descripcion, Estado)
+
+
+----ACTUALIZAR DATOS---
+UPDATE P SET
+  P.Descripcion = TM.Descripcion,
+  P.Estado = TM.Estado
+FROM Dbo.TipoInquilino P
+INNER JOIN #TipoInquilinoTemp TM
+    ON P.Id_TipoInquilino = TM.Id_TipoInquilino
+
+
+----INSERTAR DATOS---
+
+SET IDENTITY_INSERT dbo.TipoInquilino ON
+
+INSERT INTO dbo.TipoInquilino(
+Id_TipoInquilino, Descripcion, Estado)
+SELECT
+Id_TipoInquilino, Descripcion, Estado
+FROM #TipoInquilinoTemp
+
+
+EXCEPT
+SELECT
+Id_TipoInquilino, Descripcion, Estado
+FROM dbo.TipoInquilino
+
+
+SET IDENTITY_INSERT dbo.TipoInquilino OFF
+
+GO
